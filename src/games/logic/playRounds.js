af485dd109579aa50cf967a8fea19a3f94607bea
Сеня0101 <e.palicheva999@gmail.com>
2024-03-02 15:54:11 +0700

    8990

--- src/games/logic/playRounds.js
+++ src/games/logic/playRounds.js
@@ -1,13 +1,19 @@
 import readlineSync from 'readline-sync';
 import userName from '../../../bin/brain-games.js';
-import checkingАnswer from './checkingАnswer.js';
 import questionGenerator from './questionGenerator.js';
+import checkingАnswer from './checkingАnswer.js';
+
+const getUserAnswer = () => {
+  const userAnswer = readlineSync.question('Your answer: ');
+  const userNumber = Number(userAnswer);
+  return { userAnswer, userNumber };
+};
 
 const playRounds = (game) => {
   for (let i = 0; i < 3; i += 1) {
     const correct = questionGenerator(game);
-    const userAnswer = readlineSync.question('Your answer: ');
-    const userNumber = Number(userAnswer);
+    const { userAnswer, userNumber } = getUserAnswer();
+
     if (!Number.isNaN(userNumber)) {
       if (!checkingАnswer(userNumber, correct)) {
         return;
